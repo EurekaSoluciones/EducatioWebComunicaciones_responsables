bf5414598d7df69d98ac2e169871eeddab3ccf8e
EmmaDolz <emmadolz@Emma.Emma>
2026-03-31 09:21:18 -0300

    prueba para online 2

--- app/Http/Controllers/ComunicacionEController.php
+++ app/Http/Controllers/ComunicacionEController.php
@@ -36,7 +36,7 @@ class ComunicacionEController extends Controller
     ];
 
     //dd($comunicacionese[0]->tipo);
-    dd('hola'); 
+    // dd('hola'); 
 
     return view('comunicacionese.indexa', compact('alumno', 'responsable', 'comunicacionese', 'filtros'));
   }
