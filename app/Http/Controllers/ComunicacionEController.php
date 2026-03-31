269f6aefa3c404a82d1683bf6ec2bf89c0dd8b14
EmmaDolz <emmadolz@Emma.Emma>
2026-03-31 08:55:47 -0300

    coms E

--- app/Http/Controllers/ComunicacionEController.php
+++ app/Http/Controllers/ComunicacionEController.php
@@ -35,7 +35,7 @@ class ComunicacionEController extends Controller
             'filtrado' => 0,
         ];
 
-        //  dd($comunicacionese[0]->destinatario_web_user);
+        //  dd($comunicacionese[0]->tipo);
 
         return view('comunicacionese.indexa', compact('alumno', 'responsable', 'comunicacionese', 'filtros'));
     }
